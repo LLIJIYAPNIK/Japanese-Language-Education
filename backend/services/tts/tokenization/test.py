aaa69e4d7d7a41a3ef8bc88952692b58005d17ac
LLIJIYAPNIK <kas4wot@yandex.ru>
2025-09-05 15:57:09 +0300

    Added client for working with sudachipy

--- backend/services/tts/tokenization/test.py
+++ backend/services/tts/tokenization/test.py
@@ -7,15 +7,17 @@ morphemes = tokenizer.tokenize("国会議事堂前駅")
 print(morphemes[0].surface())  # '国会議事堂前駅'
 print(morphemes[0].reading_form())  # 'コッカイギジドウマエエキ'
 print(morphemes[0].part_of_speech())  # ['名詞', '固有名詞', '一般', '*', '*', '*']
+print([m.surface() for m in morphemes])  # ['国会', '議事', '堂', '前', '駅']
 
-morphemes = tokenizer.tokenize("国会議事堂前駅", SplitMode.A)
+tokenizer = Dictionary(dict="full").create()
+morphemes = tokenizer.tokenize("国会議事堂前駅", SplitMode.C)
 print([m.surface() for m in morphemes])  # ['国会', '議事', '堂', '前', '駅']
 
 
 from tdmelodic.nn.lang.mecab.unidic import UniDic
 
-u = UniDic(
-    unidic_path=r"",
-    mecabrc_path=r""
-)
-u.get_n_best("深層学習", "しんそうがくしゅう", 3)
+u = UniDic()
+for m in morphemes:
+    print(m.surface())
+    print(u.get_n_best(m.surface(), m.reading_form())[0][0][0])
+print(u.get_n_best(morphemes[0].surface(), morphemes[0].reading_form())[0][0][0])
